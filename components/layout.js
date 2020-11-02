fd5927eb8ef5302167dc305719867135472208a0
daletom <tom@imgix.com>
2020-11-02 16:46:38 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Learn how to add Image Optimization using Next.js 10"
         />
         <meta
           property="og:image"
