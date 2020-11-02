1e0c91e14dc517ed8c90e6add85dd05953276c08
daletom <tom@imgix.com>
2020-11-02 13:03:50 -0500

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import Image from 'next/image'
+function ImageContainer() {
+  return (
+    <>
+      <Image
+        src="/images/beach_zeek.jpg"
+        alt="Image"
+        width={800}
+        height={1000}
+        loading="eager"
+        quality={35}
+        sizes="(min-width: 725px) 725px, 100vw"
+      />
+    </>
+  )
+}
+export default ImageContainer
+
+/*export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}*/
