612a7613025597ce8ac7b6e85725f3fd3a3512af
daletom <tom@imgix.com>
2020-11-02 16:24:26 -0500

    adding flex colums rows and images

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -12,9 +13,91 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Tom Dale</p>
         <h1 className="title">
-        Read <Link href="/posts/first-post"><a>this page!</a></Link>
+        Read <Link href="https://dev.to/daletom"><a>original post!</a></Link>
         </h1>
       </section>
+      <div class="row">
+        <div class="column">
+          <Image
+            src="/images/beach_zeek.jpg"
+            alt="Image"
+            width={800}
+            height={800}
+            loading="lazy"
+            quality={35}
+            sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+          />
+          <Image
+            src="/images/disney.jpg"
+            alt="Image"
+            width={800}
+            height={800}
+            loading="lazy"
+            quality={35}
+            sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+          />
+        </div>
+        <div class="column">
+          <Image
+              src="/images/geiranger.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+            <Image
+              src="/images/surat-thani.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+        </div>
+        <div class="column">
+          <Image
+              src="/images/terrarium.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+            <Image
+              src="/images/zeekgraffiti.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+        </div>
+        <div class="column">
+          <Image
+              src="/images/zeek_yellow.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+            <Image
+              src="/images/kids_skateboard.jpg"
+              alt="Image"
+              width={800}
+              height={800}
+              loading="lazy"
+              quality={35}
+              sizes="(max-width: 600px) 100vw, (max-width: 1023px) 48vw, 23vw"
+            />
+        </div>
+      </div>
     </Layout>
   )
 }
